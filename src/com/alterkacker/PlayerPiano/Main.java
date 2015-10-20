c7666fc2f4456605d8af2a874c1cbf910e7af516
mblum <mikedblum@gmail.com>
2015-10-20 16:03:57 -0600

    Start of processing input file

--- src/com/alterkacker/PlayerPiano/Main.java
+++ src/com/alterkacker/PlayerPiano/Main.java
@@ -7,11 +7,24 @@ import javax.sound.midi.Synthesizer;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.InputStream;
+import java.io.*;
 
 public class Main {
     public static void main(String args[]) {
-        InputStream iss = Main.class.getClassLoader().getResourceAsStream("/rolls/1");
-        System.out.println(iss );
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("rolls/1");
+        BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(is)));
+        // BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath)));
+        String aline;
+        int n = 1;
+        try{
+            while ((aline = br.readLine()) != null) {
+                System.out.println("" + n++ + aline);
+            }
+        } catch (IOException e) {}
+        finally {
+            try {
+                br.close();
+            } catch (IOException e) {}
+        }
     }
 }
