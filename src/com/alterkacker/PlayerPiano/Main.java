3d44443cdd5b20e001a7d62c58b635bf5dc0c6e4
mblum <mikedblum@gmail.com>
2015-10-20 15:21:29 -0600

    NotePlay added

--- src/com/alterkacker/PlayerPiano/Main.java
+++ src/com/alterkacker/PlayerPiano/Main.java
@@ -7,34 +7,11 @@ import javax.sound.midi.Synthesizer;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.InputStream;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Synthesizer synth = MidiSystem.getSynthesizer();
-            synth.open();
-            final MidiChannel[] mc = synth.getChannels();
-            Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
-            synth.loadInstrument(instr[0]);
-
-            JFrame frame = new JFrame("Sound1");
-            JPanel pane = new JPanel();
-            JButton button1 = new JButton("Click me!");
-            frame.getContentPane().add(pane);
-            pane.add(button1);
-            frame.pack();
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-
-            button1.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    mc[0].noteOn(60, 600);
-                }
-            });
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace());
-        }
+    public static void main(String args[]) {
+        InputStream iss = Main.class.getClassLoader().getResourceAsStream("/rolls/1");
+        System.out.println(iss );
     }
-
-
 }
---
+++ src/com/alterkacker/PlayerPiano/NotePlay.java
@@ -0,0 +1,9 @@
+package com.alterkacker.PlayerPiano;
+
+/**
+ * Created by mblum on 10/20/15.
+ */
+public class NotePlay {
+    int noteNumber;
+    int noteValue;
+}
