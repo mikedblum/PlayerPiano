707974b7b5699d57627b4200df6a87a871aea316
mblum <mikedblum@gmail.com>
2015-10-24 21:33:06 -0600

    - fixed key spec. / accidentals - fleshed out 3rds - restored HardTimes

--- src/com/alterkacker/PlayerPiano/RollParser.java
+++ src/com/alterkacker/PlayerPiano/RollParser.java
@@ -26,7 +26,7 @@ public class RollParser {
                 nLines++;
                 aline = aline.trim().replaceAll(" +", " ").toLowerCase();
                 if (!(aline.startsWith("#") || aline.length() == 0)) {
-                    if (tempoLine){
+                    if (tempoLine) {
                         parseSigLine(aline, nLines);
                         tempoLine = false;
                     } else {
@@ -38,27 +38,26 @@ public class RollParser {
         } finally {
             try {
                 br.close();
-            } catch (IOException e) {}
+            } catch (IOException e) {
+            }
         }
+        http:
+//www.linuxmint.com/start/katya/
 
         return notes;
     }
 
-    private static void parseSigLine(String aline, int nLines){
+    private static void parseSigLine(String aline, int nLines) {
         String flds[] = aline.split(" ");
         qtrMsec = Integer.valueOf(flds[0]);
-        if (flds.length > 1){
-            for (int ix=1; ix<flds.length; ix++) {
+        if (flds.length > 1) {
+            for (int ix = 1; ix < flds.length; ix++) {
                 String fld = flds[ix];
-                if (fld.length() == 2) {
-                    fld = fld.substring(0, 1) + "4" + fld.substring(1);
-                }
                 String noteLtr = fld.substring(0, 1);
-                String noteOctave = fld.substring(1, 2);
-                String noteAcc = fld.substring(2);
+                String noteAcc = fld.substring(1, 2);
                 String tNote = noteLtr + noteAcc;
                 if (validSigNotes.contains(tNote)) {
-                    sigAccs.put(noteLtr + noteOctave, noteAcc);
+                    sigAccs.put(noteLtr, noteAcc);
                 } else {
                     System.err.println("Invalid note spec '" + fld + "' in signature line (" + nLines + ")");
                 }
@@ -66,33 +65,36 @@ public class RollParser {
         }
     }
 
-    private static void parseAline(String aline, int nLines){
+    private static void parseAline(String aline, int nLines) {
         String measures[] = aline.split("\\|");
         int nMeasures = measures.length;
-        for (int mx = 0; mx < nMeasures; mx++){
+        for (int mx = 0; mx < nMeasures; mx++) {
             String measure = measures[mx];
             msrAccs.clear();
-            msrAccs.putAll(sigAccs);
             String specs[] = measure.trim().split(" +");
             int nSpecs = specs.length;
-            for (int sx=0; sx < nSpecs; sx++){
+            for (int sx = 0; sx < nSpecs; sx++) {
                 String spec = specs[sx];
-                NoteSpec nspec = new NoteSpec(spec, (mx == nMeasures-1 && sx == nSpecs-1));
-                String noteAndAcc = nspec.noteLtr;
+                NoteSpec nspec = new NoteSpec(spec, (mx == nMeasures - 1 && sx == nSpecs - 1));
+                String noteAndAcc = nspec.noteLtr + ((nspec.noteAcc != null)? nspec.noteAcc: "");
                 String noteAndOctave = nspec.noteLtr + String.valueOf(nspec.noteOctave);
-                if (nspec.noteAcc != null) {
-                    noteAndAcc += nspec.noteAcc;
-                    if (!"n".equals(nspec.noteAcc) && !msrAccs.containsKey(noteAndOctave))
-                        msrAccs.put(noteAndOctave, nspec.noteAcc);
-                }
+
                 if (validNotes.contains(noteAndAcc)) {
                     NoteInfo nplay = new NoteInfo(nspec, qtrMsec);
-                    if (nspec.noteAcc == null && msrAccs.containsKey(noteAndOctave)){
-                        nplay.noteNumber += msrAccs.get(noteAndOctave).equals("#")? 1: -1;
+                    // note with no accidental
+                    if (nspec.noteAcc == null) {
+                        if (sigAccs.containsKey(nspec.noteLtr)) {
+                            nplay.noteNumber += sigAccs.get(nspec.noteLtr).equals("#") ? 1 : -1;
+                        } else if (msrAccs.containsKey(noteAndOctave)) {
+                            nplay.noteNumber += msrAccs.get(noteAndOctave).equals("#") ? 1 : -1;
+                        }
+                    // note with accidental
+                    } else if (!"n".equals(nspec.noteAcc) && !msrAccs.containsKey(noteAndOctave)){
+                        msrAccs.put(noteAndOctave, nspec.noteAcc);
                     }
                     notes.add(nplay);
                 } else {
-                    System.err.println("Invalid note spec '"+spec+"' in music line ("+nLines+")");
+                    System.err.println("Invalid note spec '" + spec + "' in music line (" + nLines + ")");
                 }
             }
         }
