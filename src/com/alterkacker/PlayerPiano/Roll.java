ecc3999ee64275ab8c4339827542cc9104e9703e
mblum <mikedblum@gmail.com>
2015-10-28 20:45:54 -0600

    started Roll.java

---
+++ src/com/alterkacker/PlayerPiano/Roll.java
@@ -0,0 +1,11 @@
+package com.alterkacker.PlayerPiano;
+
+import java.util.List;
+
+/**
+ * Created by mblum on 10/28/15.
+ */
+public class Roll {
+    List<NoteInfo> notes;
+    List<List<NoteSpec>> specs;
+}
